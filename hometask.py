a003d4ec8eb8f031683b97652b21d50d7d637fa7
Insharah Khan <74257117+InsharahKhann@users.noreply.github.com>
2020-11-11 22:18:54 +0500

    Add files via upload

---
+++ hometask.py
@@ -0,0 +1,15 @@
+           # ROLL NO. 008
+'''
+write a python program to check if a number is positive, negative or zero?
+'''
+
+number = int(input("Enter the number is: "))
+
+if (number == 0):
+    print ("the given number is zero") 
+
+if (number > 0):
+    print ("the given number is positive: ")
+
+if (number < 0):
+    print ("the given number is negative: ")
