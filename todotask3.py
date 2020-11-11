f36492d59a66c604cfd293f086187af42d48f979
Insharah Khan <74257117+InsharahKhann@users.noreply.github.com>
2020-11-11 22:22:55 +0500

    Add files via upload

---
+++ todotask3.py
@@ -0,0 +1,12 @@
+                #ROLL NO.008
+'''
+Write a Python program to convert Celsius
+temperatures to Fahrenheit.
+'''
+
+# (°C × 9/5) + 32 = °F
+
+celsius = int(input("enter temperature is: "))
+fahrenheit = (celsius * (9 / 5)) + 32
+
+print ("the temperature in fahrenheit is: ", fahrenheit)
