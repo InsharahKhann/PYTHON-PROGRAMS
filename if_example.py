f3e2f3c6c1e06ee4c42b7b0964cbb728e4a6548a
Insharah Khan <74257117+InsharahKhann@users.noreply.github.com>
2020-11-11 22:19:58 +0500

    Add files via upload

---
+++ if_example.py
@@ -0,0 +1,11 @@
+ali_age = int(input ('Input ali age:  '))
+sara_age = int(input ('Input Sara age:  '))
+
+if ali_age == sara_age:
+    print ('They both have same age')
+
+if ali_age < sara_age:
+    print ('ali is younger than Sara')
+
+if ali_age > sara_age:
+    print ('ali is elder than sara')
