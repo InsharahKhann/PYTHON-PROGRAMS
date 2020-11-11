5c1c4eb8a1088c49503ccde1841581bc10ec1f62
Insharah Khan <74257117+InsharahKhann@users.noreply.github.com>
2020-11-11 22:19:31 +0500

    Add files via upload

---
+++ if.py
@@ -0,0 +1,9 @@
+ali = input('Is ali present Today? (y/n)')  # y / n
+sara = input('Is sara present Today? (y/n)') # y / n
+
+# if (true)
+
+if (ali == "y") or (sara == "y"):
+    print ("They both are present")
+
+
