6651fd4186d5a0ea5b9586cf06fea51475ffbda2
Insharah Khan <74257117+InsharahKhann@users.noreply.github.com>
2020-11-11 22:20:47 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+#roll no.008
+name = input("Enter name: ")
+father_name = input("Enter father_name: ")
+age = int(input("Enter age: "))
+
+if (age == 11) or (age == 21) or (age == 31):
+    print(" your age is ", age )
